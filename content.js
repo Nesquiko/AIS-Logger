f3ba4f0526a591b32c0a8ff3ba902113ff52b57b
Lukas Castven <lukascastven@gmail.com>
2021-07-19 16:05:15 +0200

    Store and enter credentials.
    When https://is.stuba.sk/auth is opened, automatically enter stored
    username and password. If user wants to store them for the first time,
    clicking extension icon will open a popup with two text fields for
    username and password in which user can enter them and click on Save
    button to store them.

--- content.js
+++ content.js
@@ -1,1 +1,21 @@
-alert('Grrrr')
+
+chrome.storage.sync.get(['uname'], function(result) {
+	document.getElementById('credential_0').value = result.uname
+//	console.log('Uname = ' + result.uname );
+});
+
+chrome.storage.sync.get(['password'], function(result) {
+	document.getElementById('credential_1').value = result.password
+//	console.log('Password = ' + result.password );
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {	
+
+	chrome.storage.sync.set({'uname': request[0]}, function() {
+//		  console.log('Username is set to ' + request[0]);
+	});
+
+	chrome.storage.sync.set({'password': request[1]}, function() {
+//		  console.log('Password is set to ' + request[1]);
+	});
+})
---
+++ popup.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('button').addEventListener('click', onclick, false)
+	
+
+	function onclick () {
+		var uname = document.getElementById('username').value
+
+		var password = document.getElementById('password').value
+
+		chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, [uname, password])
+		})
+	}
+
+}, false)
