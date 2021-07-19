82d82266cdcbf58a2a0898af175d3bcc6c11acf0
Lukas Castven <lukascastven@gmail.com>
2021-07-19 17:03:35 +0200

    Comment and delete logs.

--- content.js
+++ content.js
@@ -1,21 +1,16 @@
 
+// Upon landing on the https://is.stuba.sk/auth, this will populate username textfield
 chrome.storage.sync.get(['uname'], function(result) {
 	document.getElementById('credential_0').value = result.uname
-//	console.log('Uname = ' + result.uname );
 });
 
-chrome.storage.sync.get(['password'], function(result) {
+// This will populate password text field
+chrome.storage.sync.get(['password'], function(result) { 
 	document.getElementById('credential_1').value = result.password
-//	console.log('Password = ' + result.password );
 });
 
+/** When Save button in popup.html is clicked this will execute */
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {	
-
-	chrome.storage.sync.set({'uname': request[0]}, function() {
-//		  console.log('Username is set to ' + request[0]);
-	});
-
-	chrome.storage.sync.set({'password': request[1]}, function() {
-//		  console.log('Password is set to ' + request[1]);
-	});
+	chrome.storage.sync.set({'uname': request[0]})
+	chrome.storage.sync.set({'password': request[1]});
 })
