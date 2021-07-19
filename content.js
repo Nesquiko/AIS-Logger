cae140c47296f13a5cb96d20682240bd4259f079
Lukas Castven <lukascastven@gmail.com>
2021-07-19 21:48:23 +0200

    Format with Prettier and add id to button.

--- content.js
+++ content.js
@@ -1,16 +1,15 @@
-
 // Upon landing on the https://is.stuba.sk/auth, this will populate username textfield
-chrome.storage.sync.get(['uname'], function(result) {
-	document.getElementById('credential_0').value = result.uname
+chrome.storage.sync.get(["uname"], function (result) {
+    document.getElementById("credential_0").value = result.uname;
 });
 
 // This will populate password text field
-chrome.storage.sync.get(['password'], function(result) { 
-	document.getElementById('credential_1').value = result.password
+chrome.storage.sync.get(["password"], function (result) {
+    document.getElementById("credential_1").value = result.password;
 });
 
 /** When Save button in popup.html is clicked this will execute */
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {	
-	chrome.storage.sync.set({'uname': request[0]})
-	chrome.storage.sync.set({'password': request[1]});
-})
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    chrome.storage.sync.set({ uname: request[0] });
+    chrome.storage.sync.set({ password: request[1] });
+});
--- popup.js
+++ popup.js
@@ -1,15 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-	document.querySelector('button').addEventListener('click', onclick, false)
-	
+document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+        document
+            .getElementById("save-btn")
+            .addEventListener("click", onclick, false);
 
-	function onclick () {
-		var uname = document.getElementById('username').value
+        function onclick() {
+            var uname = document.getElementById("username").value;
 
-		var password = document.getElementById('password').value
+            var password = document.getElementById("password").value;
 
-		chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, [uname, password])
-		})
-	}
-
-}, false)
+            chrome.tabs.query(
+                { currentWindow: true, active: true },
+                function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, [uname, password]);
+                }
+            );
+        }
+    },
+    false
+);
