bde082ead6ee7391cc9bced5c09ce03bbd2eb413
Lukas Castven <lukascastven@gmail.com>
2021-07-29 21:58:30 +0200

    Version 2.0. Add On Off toggle.
    Added toggle with wich user can turn on and off the extension.

--- content.js
+++ content.js
@@ -1,15 +1,20 @@
-// Upon landing on the https://is.stuba.sk/auth, type username into textfield
-chrome.storage.sync.get(["uname"], function (result) {
-    document.getElementById("credential_0").value = result.uname;
-});
+chrome.storage.sync.get(["isOff"], function (result) {
+    if (!result.isOff) {
+        // Upon landing on the https://is.stuba.sk/auth, type username into
+        // textfield.
+        chrome.storage.sync.get(["uname"], function (result) {
+            document.getElementById("credential_0").value = result.uname;
+        });
 
-// Type password into textfield
-chrome.storage.sync.get(["password"], function (result) {
-    document.getElementById("credential_1").value = result.password;
-});
+        // Type password into textfield.
+        chrome.storage.sync.get(["password"], function (result) {
+            document.getElementById("credential_1").value = result.password;
+        });
 
-// And click login button. (Without timeout the webpage doesn't register
-// entered username properly and displays error message.)
-setTimeout(function () {
-    document.getElementById("login-btn").click();
-}, 100);
+        // And click login button. Without timeout the webpage doesn't register
+        // entered username properly and displays error message.
+        setTimeout(function () {
+            document.getElementById("login-btn").click();
+        }, 100);
+    }
+});
--- popup.js
+++ popup.js
@@ -1,19 +1,40 @@
 document.addEventListener(
     "DOMContentLoaded",
     function () {
+        // Find save button and add on click listener.
         document
             .getElementById("save-btn")
             .addEventListener("click", onclick, false);
 
-        // This function runs after the save button is clicked.
+        // On click listener of save button, retrieves username and password
+        // and saves them.
         function onclick() {
             var username = document.getElementById("username").value;
-
             var pswd = document.getElementById("password").value;
 
             chrome.storage.sync.set({ uname: username });
-
             chrome.storage.sync.set({ password: pswd });
+        }
+
+        // Find toggle switch.
+        const toggle = document.querySelector(".switch input");
+
+        // Set toggle.checked to previosly selected value./
+        chrome.storage.sync.get(["isOff"], function (result) {
+            toggle.checked = result.isOff;
+        });
+
+        // Fist time the extension is opened, no isOff value is registered,
+        // so this will set it up. In next runs this will not cause any issues,
+        // it will only set the value to same one.
+        chrome.storage.sync.set({ isOn: toggle.checked });
+
+        // add on change listener to toggle.
+        toggle.addEventListener("change", onToggleChange, false);
+
+        // On change listener of toggle, saves current state of toggle.
+        function onToggleChange() {
+            chrome.storage.sync.set({ isOff: toggle.checked });
         }
     },
     false
